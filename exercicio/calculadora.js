a7e607b78ed4cb5aefdb6c5af1d20a77c1aa1342
MatheusAraujo <matheus.praciano@iesb.edu.br>
2025-08-27 20:38:23 -0300

    Calculadora exercicio

---
+++ exercicio/calculadora.js
@@ -0,0 +1,32 @@
+function CalSomar(so1, so2){
+    return so1+so2
+}
+
+function CalSub(su1, su2){
+    return su1-su2
+}
+
+function CalMult(mul1, mult2){
+    return mul1*mult2
+}
+
+function CalDiv(div1, div2){
+    return div1/div2
+}
+
+function CalQua(qua1){
+    return qua1*qua1
+}
+
+function CalRaiz(raiz1){
+    return Math.sqrt(raiz1)
+}
+
+module.exports = {
+    CalSomar,
+    CalSub,
+    CalMult,
+    CalDiv,
+    CalQua,
+    CalRaiz
+} 
---
+++ exercicio/index.js
@@ -0,0 +1,16 @@
+
+const calc = require('./calculadora');
+
+console.log('3 + 4 =', calc.CalSomar(3, 4));
+
+console.log('10 - 7 =', calc.CalSub(10, 7));
+
+console.log('6 * 5 =', calc.CalMult(6, 5));
+
+console.log('8 / 2 =', calc.CalDiv(8, 2));
+
+console.log('5 ao quadrado =', calc.CalQua(5));
+
+console.log('raiz quadrada de 25 =', calc.CalRaiz(25));
+
+
